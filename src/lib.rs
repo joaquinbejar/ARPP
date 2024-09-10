994c99effe64cb92756353757fc10376862fb97b
Joaquin Bejar <jb@taunais.com>
2024-09-10 11:36:00 +0200

    Normalize comments and package names
    Standardized the comment formatting across multiple source files and changed the package name from "ARPP" to "arpp" in Cargo.toml for consistency. Added several dependencies to the Cargo.toml for future development.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 /******************************************************************************
-    Author: Joaquín Béjar García
-    Email: jb@taunais.com 
-    Date: 10/9/24
- ******************************************************************************/
+   Author: Joaquín Béjar García
+   Email: jb@taunais.com
+   Date: 10/9/24
+******************************************************************************/
 
-pub mod arpp;
-pub mod simulation;
 pub mod analysis;
+pub mod arpp;
 pub mod cli;
+pub mod simulation;
 pub mod utils;
