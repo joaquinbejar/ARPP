b31959cd78e7cd2a35ef00127d80edd02dd7bed6
Joaquin Bejar <jb@taunais.com>
2024-09-12 12:41:11 +0200

    Refactor monte_carlo.rs for better structure and clarity
    Reorganized imports, added detailed doc comments, and changed error logging levels. This cleanup improves code readability and maintainability, ensuring better documentation for the MonteCarloSimulation struct and its methods.

--- examples/simulation_and_visualize.rs
+++ examples/simulation_and_visualize.rs
@@ -1,17 +1,19 @@
 /******************************************************************************
-    Author: Joaquín Béjar García
-    Email: jb@taunais.com 
-    Date: 10/9/24
- ******************************************************************************/
+   Author: Joaquín Béjar García
+   Email: jb@taunais.com
+   Date: 10/9/24
+******************************************************************************/
+use arpp::analysis::metrics::analyze_simulation_results;
+use arpp::analysis::visualization::{
+    create_metrics_chart, create_price_chart, create_simulation_analysis_chart,
+};
+use arpp::arpp::liquidity_pool::LiquidityPool;
+use arpp::simulation::monte_carlo::MonteCarloSimulation;
+use arpp::simulation::strategies::MeanReversionStrategy;
+use arpp::utils::logger::setup_logger;
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
 use tracing::info;
-use arpp::utils::logger::setup_logger;
-use arpp::arpp::liquidity_pool::LiquidityPool;
-use arpp::simulation::monte_carlo::MonteCarloSimulation;
-use arpp::simulation::strategies::{MeanReversionStrategy, RandomStrategy};
-use arpp::analysis::metrics::analyze_simulation_results;
-use arpp::analysis::visualization::{create_price_chart, create_metrics_chart, create_simulation_analysis_chart};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -19,19 +21,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let alpha = dec!(0.2);
     let beta = dec!(5);
 
-    let mut initial_pool = LiquidityPool::new(
-        dec!(100000),  // token_a
-        dec!(100000),  // token_b
-        dec!(100),       // p_ref
-        alpha,     // alpha
-        beta,       // beta
+    let initial_pool = LiquidityPool::new(
+        dec!(100000), // token_a
+        dec!(100000), // token_b
+        dec!(100),    // p_ref
+        alpha,        // alpha
+        beta,         // beta
     );
 
     // let strategy = Box::new(RandomStrategy::new(0.5, dec!(10)));
     let strategy = Box::new(MeanReversionStrategy::new(dec!(0.1), dec!(100)));
-    let iterations = 1000;
-    let steps_per_iteration = 10;
-
+    let iterations = 5000;
+    let steps_per_iteration = 2;
 
     let mut simulation = MonteCarloSimulation::new(
         initial_pool.clone(),
@@ -39,31 +40,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         steps_per_iteration,
         strategy,
         Decimal::new(1, 1),
-        Decimal::new(1, 2)
+        Decimal::new(1, 2),
     );
 
     let result = simulation.run().await?;
 
     info!("Simulation completed");
     info!("Average price change: {:.4}", result.average_price_change);
-    info!("Average liquidity change: {:.4}", result.average_liquidity_change);
+    info!(
+        "Average liquidity change: {:.4}",
+        result.average_liquidity_change
+    );
     info!("Max price: {:.4}", result.max_price);
     info!("Min price: {:.4}", result.min_price);
 
-
     let price_history = result.metrics.get_prices();
     let p_ref_history = result.metrics.get_p_ref();
-    create_price_chart(&price_history, &p_ref_history, "draws/price_chart.png",alpha, beta)?;
+    create_price_chart(
+        &price_history,
+        &p_ref_history,
+        "draws/price_chart.png",
+        alpha,
+        beta,
+    )?;
     info!("Price chart created: draws/price_chart.png");
 
     let metrics_history = simulation.get_metrics_history();
     create_metrics_chart(&metrics_history, "draws/metrics_chart.png")?;
     info!("Metrics chart created: draws/metrics_chart.png");
 
-    let final_pool = simulation.get_final_pool();
     let pool_metrics = result.clone().metrics;
     let analysis = analyze_simulation_results(&result);
-    create_simulation_analysis_chart(&analysis, "draws/simulation_analysis_chart.png", alpha, beta)?;
+    create_simulation_analysis_chart(
+        &analysis,
+        "draws/simulation_analysis_chart.png",
+        alpha,
+        beta,
+    )?;
     info!("Simulation analysis chart created: draws/simulation_analysis_chart.png");
 
     info!("\nFinal Pool Metrics:");
